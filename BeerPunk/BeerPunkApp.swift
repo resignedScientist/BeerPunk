2ccf2fcbb2e63ca14bb311e96bab34aa2783ac87
Norman Laudien <laudien@adesso-mobile.de>
2022-11-28 21:18:52 +0100

    Initial Commit

---
+++ BeerPunk/BeerPunkApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeerPunkApp.swift
+//  BeerPunk
+//
+//  Created by Laudien, Norman on 28.11.22.
+//
+
+import SwiftUI
+
+@main
+struct BeerPunkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
