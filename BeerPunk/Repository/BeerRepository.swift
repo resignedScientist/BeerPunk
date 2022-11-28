79d99be9ffcb518987bb162ee1d8011382edd293
Norman Laudien <laudien@adesso-mobile.de>
2022-11-28 22:55:56 +0100

    implement BeerRepository

--- BeerPunk/Repository/BeerRepository.swift
+++ BeerPunk/Repository/BeerRepository.swift
@@ -7,9 +7,42 @@
 
 import Foundation
 
+enum Constants {
+    static let baseURL = "https://api.punkapi.com/v2/beers"
+}
+
 protocol BeerRepository {
     
     func fetchBeers() async throws -> [Beer]
     
-    func fetchBeer() async throws -> Beer
+    func fetchBeer(id: Int) async throws -> Beer
+}
+
+class BeerRepositoryImpl: BeerRepository {
+    
+    func fetchBeers() async throws -> [Beer] {
+        guard let url = URL(string: Constants.baseURL) else {
+            throw BeerError.unknown
+        }
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let decoder = JSONDecoder()
+        
+        return try decoder.decode([Beer].self, from: data)
+    }
+    
+    func fetchBeer(id: Int) async throws -> Beer {
+        guard let url = URL(string: "\(Constants.baseURL)/\(id)") else {
+            throw BeerError.unknown
+        }
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let decoder = JSONDecoder()
+        
+        return try decoder.decode(Beer.self, from: data)
+    }
+}
+
+enum BeerError: LocalizedError {
+    case unknown
 }
