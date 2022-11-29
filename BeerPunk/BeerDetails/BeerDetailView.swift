9b067e5c36c26baa4379b1cfb65b85725bd14d1a
Norman Laudien <laudien@adesso-mobile.de>
2022-11-29 16:02:30 +0100

    add tagline to view

--- BeerPunk/BeerDetails/BeerDetailView.swift
+++ BeerPunk/BeerDetails/BeerDetailView.swift
@@ -18,9 +18,16 @@ struct BeerDetailView: View {
             VStack {
                 
                 HStack(alignment: .top) {
+                    
                     AsyncImage(url: viewModel.beer.imageURL, scale: 3)
                         .padding(.trailing)
-                    Text(viewModel.beer.description)
+                    
+                    VStack(alignment: .leading) {
+                        Text(viewModel.beer.tagline)
+                            .font(.headline)
+                            .padding(.bottom)
+                        Text(viewModel.beer.description)
+                    }
                 }
                 
                 
@@ -36,7 +43,7 @@ struct BeerDetailView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
             BeerDetailView(
-                viewModel: BeerDetailViewModel(beer: Beer(id: 0, name: "Weißbier", tagline: "bier", firstBrewed: "2000 vor Christus", description: "Ein super tolles Weißbier. Der Klassiker.", imageURL: URL(string: "https://images.punkapi.com/v2/192.png")!, volume: Volume(value: 5, unit: "Liter"), boilVolume: Volume(value: 10, unit: "Liter"), foodPairing: ["Weißwurscht"], brewersTips: "Mit Brezel und Weißwurscht genießen", contributor: "Unbekannt"))
+                viewModel: BeerDetailViewModel(beer: Beer(id: 0, name: "Weißbier", tagline: "A real bitter experience", firstBrewed: "2000 vor Christus", description: "Ein super tolles Weißbier. Der Klassiker.", imageURL: URL(string: "https://images.punkapi.com/v2/192.png")!, volume: Volume(value: 5, unit: "Liter"), boilVolume: Volume(value: 10, unit: "Liter"), foodPairing: ["Weißwurscht"], brewersTips: "Mit Brezel und Weißwurscht genießen", contributor: "Unbekannt"))
             )
         }
     }
